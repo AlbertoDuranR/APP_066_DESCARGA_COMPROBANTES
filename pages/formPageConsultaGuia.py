d9bb340e3a8283c21d7f4e149236e49afe425f92
AlbertoDuranR <teogenes.duran@terranovatrading.com.pe>
2025-08-12 17:03:56 -0500

    Add : llegar hasta tabla

--- pages/formPageConsultaGuia.py
+++ pages/formPageConsultaGuia.py
@@ -1,5 +1,6 @@
 from utils.waits import waitVisible, waitClickable
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class FormPage:
     def goToForm(self, driver):
@@ -21,4 +22,28 @@ class FormPage:
         driver.execute_script("arguments[0].click();", btn_siguiente)
 
         # 2. Criterios de búsqueda
-        
+
+        # 1) Seleccione el tipo de GRE (clic al label para evitar input oculto)
+        lblTipo = waitClickable(driver, (By.CSS_SELECTOR, f"label.form-check-label[for='09']"))
+        driver.execute_script("arguments[0].scrollIntoView({block:'center'});", lblTipo)
+        driver.execute_script("arguments[0].click();", lblTipo)
+
+        # 2) RUC del emisor
+        rucEmisor = "20100055237"
+        inpRuc = waitVisible(driver, (By.CSS_SELECTOR, "input#rucEmisor[formcontrolname='rucEmisor']"))
+        inpRuc.clear(); inpRuc.send_keys(rucEmisor); inpRuc.send_keys(Keys.TAB)
+
+        # 3) Serie
+        serie = "T100"
+        inpSerie = waitVisible(driver, (By.CSS_SELECTOR, "input[formcontrolname='serie']"))
+        inpSerie.clear(); inpSerie.send_keys(serie); inpSerie.send_keys(Keys.TAB)
+
+        # 4) Número
+        numero = "94565"
+        inpNumero = waitVisible(driver, (By.CSS_SELECTOR, "input[formcontrolname='numero']"))
+        inpNumero.clear(); inpNumero.send_keys(numero); inpNumero.send_keys(Keys.TAB)
+
+        # 5) Click en Siguiente (espera a que se habilite)
+        btnSiguiente = waitClickable(driver, (By.CSS_SELECTOR, "button[type='submit']:not([disabled])"))
+        driver.execute_script("arguments[0].scrollIntoView({block:'center'});", btnSiguiente)
+        driver.execute_script("arguments[0].click();", btnSiguiente)
